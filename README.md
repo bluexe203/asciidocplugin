67f292f2b5da7425933c03559d6ee0444e22e381
bluexe203 <bluexe203@gmail.com>
2021-10-20 13:29:21 +0900

    日本語のhrefとIDのエスケープ処理を追加

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # asciidocplugin
 
+VSCode用のAsciidocプレビュー用のプラグインです。
+
+オフラインでasciidocとmermaidの図表の描画を行います。
+
+以下のバージョンのライブラリを利用しています。
+
+[asciidoctor.js : 2.2.5](https://github.com/asciidoctor/asciidoctor.js/)
+[mermaid : 7.0.10](https://github.com/mermaid-js/mermaid)
+
+# 使い方
+
+asciidocplugin-0.0.1.vsixをインストールしてください。
+
+asciidocのファイル(*.adoc)を開いて右上のPreview AsciiDocを押下するとプレビューが表示されます。
+
+プレビュー画面はadocの変更の度に再レンダリングされます。
+
+# mermaidの構文
+
+以下の用に記載します。(korokiのmermaid用の構文と同じですが....のブロックでしか判別をしていません)
+
+```
+[mermaid]
+....
+sequenceDiagram
+	クライアント-->>サーバ:要求;
+    サーバ-->>クライアント:応答;
+....
+```
+
--- src/extension.ts
+++ src/extension.ts
@@ -10,15 +10,14 @@ let _panelAlive = false;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	//console.log('Congratulations, your extension "asciidocplugin" is now active!');
-	const js =  "" + fs.readFileSync(path.join(context.extensionPath, 'mermaid.min.js'));
-	const cssm = fs.readFileSync(path.join(context.extensionPath, 'mermaid.min.css'));
-	const cssa = fs.readFileSync(path.join(context.extensionPath, 'asciidoctor.css'));
-	const css : string = "" + cssm + cssa;
-	let webViewPanel : vscode.WebviewPanel | null = null;
+	const js = "" + fs.readFileSync(path.join(context.extensionPath, 'mermaid.min.js'));
+	const css = "" + fs.readFileSync(path.join(context.extensionPath, 'mermaid.min.css'))
+		+ fs.readFileSync(path.join(context.extensionPath, 'asciidoctor.css'));
+	let webViewPanel: vscode.WebviewPanel | null = null;
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -27,12 +26,11 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 
-		if (!_panelAlive)
-		{
+		if (!_panelAlive) {
 			webViewPanel = vscode.window.createWebviewPanel(
-				"AsciiDoc", 
-				"AsciiDoc Browser", 
-				vscode.ViewColumn.Beside, 
+				"AsciiDoc",
+				"AsciiDoc Browser",
+				vscode.ViewColumn.Beside,
 				{
 					enableScripts: true,
 					enableFindWidget: true,
@@ -40,22 +38,21 @@ export function activate(context: vscode.ExtensionContext) {
 				}
 			);
 
-			if (webViewPanel)
-			{
-				webViewPanel.onDidDispose( () => {
+			if (webViewPanel) {
+				webViewPanel.onDidDispose(() => {
 					_panelAlive = false;
-            	});
+				});
 			}
-			
+
 			vscode.workspace.onDidChangeTextDocument(event => {
-				CreatePreviewHtml(context, webViewPanel, js ,css);
+				CreatePreviewHtml(context, webViewPanel, js, css);
 			}, null, context.subscriptions);
 
 			vscode.window.onDidChangeActiveTextEditor(event => {
-				CreatePreviewHtml(context, webViewPanel, js ,css);
+				CreatePreviewHtml(context, webViewPanel, js, css);
 			}, null, context.subscriptions);
 
-			CreatePreviewHtml(context, webViewPanel, js ,css);
+			CreatePreviewHtml(context, webViewPanel, js, css);
 			_panelAlive = true;
 		}
 	});
@@ -63,38 +60,54 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-function CreatePreviewHtml(context: vscode.ExtensionContext, webViewPanel : vscode.WebviewPanel | null, js : string, css : string)
-{
+function CreatePreviewHtml(context: vscode.ExtensionContext, webViewPanel: vscode.WebviewPanel | null, js: string, css: string) {
 	const editor = vscode.window.activeTextEditor;
 	const content = editor?.document.getText().toString();
 
-	if (content !== null && content !== undefined && webViewPanel !== null)
-	{
-		const html = asciidoctor.convert(ConvertMermaid(content));
-		webViewPanel.webview.html =
-		"<script>" + js + "</script>" +
-		"<style>" + css + "</style>" +
-		html;
-	}	
+	if (content !== null && content !== undefined && webViewPanel !== null) {
+		//webViewPanel.webview.html = "" + fs.readFileSync("z:\\test.html");
+		const html = ConvertLink(asciidoctor.convert(ConvertMermaid(content)));
+		webViewPanel.webview.html = `\
+<!DOCTYPE html>\
+<html lang="ja"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">\
+<script>\
+${js}\
+</script>\
+<style>\
+${css}\
+</style>\
+<body>\
+${html}\
+</body>\
+</html>`;
+	}
 }
 
-function ConvertMermaid(str : string) : string
-{
+// 日本語のhrefとIDをエスケープする(VSCodeのWebViewPanelは<a>でリンク先が日本語のIDにジャンプ出来ない？)
+function ConvertLink(str: string): string {
+	str = str.replace(/href\="\#_(.*)?"/g,function(match, args){
+		console.log(args);
+		return `href="#_${escape(args)}"`;
+	});
+	str = str.replace(/id\="_(.*)?"/g,function(match, args){
+		return `id="_${escape(args)}"`;
+	});
+	return str;
+}
+
+// mermaidの記述をasciidocのPassThroughで囲む
+function ConvertMermaid(str: string): string {
 	var lines = str.split('\n');
 
 	let ret = "";
 	let state = 0;
-	for (let i = 0; i < lines.length; i++)
-	{
+	for (let i = 0; i < lines.length; i++) {
 		const line = lines[i];
-		if (line.indexOf("[mermaid]") === 0)
-		{
+		if (line.indexOf("[mermaid]") === 0) {
 			state++;
 		}
-		else if (line.indexOf("....") === 0)
-		{
-			switch (state)
-			{
+		else if (line.indexOf("....") === 0) {
+			switch (state) {
 				case 1:
 					ret += "\r\n++++";
 					ret += "\r\n<div class=\"mermaid\">";
@@ -107,8 +120,7 @@ function ConvertMermaid(str : string) : string
 					break;
 			}
 		}
-		else
-		{
+		else {
 			ret += line + "\n";
 		}
 	}
@@ -116,4 +128,4 @@ function ConvertMermaid(str : string) : string
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
